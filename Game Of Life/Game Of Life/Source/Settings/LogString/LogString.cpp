c48430e7900f280d9443e06d44fd26f3cc46cc13
Psycho <56837234+psychopattt@users.noreply.github.com>
2023-12-28 20:58:44 -0500

    Added automatic casting for bools and numbers in LogString

--- Game Of Life/Game Of Life/Source/Settings/LogString/LogString.cpp
+++ Game Of Life/Game Of Life/Source/Settings/LogString/LogString.cpp
@@ -15,18 +15,28 @@ bool LogString::IsEmpty() const
 	return log.empty();
 }
 
-LogString& LogString::operator<<(const char* text)
+LogString& LogString::operator <<(char* text)
+{
+	return *this << const_cast<const char*>(text);
+}
+
+LogString& LogString::operator <<(const char* text)
 {
 	log += text;
 	return *this;
 }
 
-LogString& LogString::operator<<(const std::string& text)
+LogString& LogString::operator <<(const std::string& text)
 {
 	return *this << text.c_str();
 }
 
-LogString::operator const char*() const
+LogString& LogString::operator <<(bool boolean)
+{
+	return *this << (boolean ? "true" : "false");
+}
+
+LogString::operator const char* () const
 {
 	return log.c_str();
 }
--- Game Of Life/Game Of Life/Source/Settings/LogString/LogString.h
+++ Game Of Life/Game Of Life/Source/Settings/LogString/LogString.h
@@ -9,8 +9,17 @@ class LogString
 		void Clear();
 		bool IsEmpty() const;
 
+		LogString& operator <<(char* text);
 		LogString& operator <<(const char* text);
 		LogString& operator <<(const std::string& text);
+		LogString& operator <<(bool boolean);
+
+		template <typename Number>
+		LogString& operator <<(Number number)
+		{
+			return *this << std::to_string(number).c_str();
+		}
+
 		operator const char* () const;
 
 	private:
